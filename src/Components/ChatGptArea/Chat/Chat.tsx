4da63adf6490d7fe017bc0e4bf187ea47480abaf
bar991 <zviely2003@gmail.com>
2024-03-25 16:25:20 +0200

    update

--- src/Components/ChatGptArea/Chat/Chat.tsx
+++ src/Components/ChatGptArea/Chat/Chat.tsx
@@ -1,21 +1,27 @@
+import { useForm } from "react-hook-form";
+import { ChatModel } from "../../../Models/ChatModel";
 import { chatGptService } from "../../../Services/ChatGptService";
+import { promptEngineering } from "../../../Services/promptEngineering";
 import "./Chat.css";
 
 export function Chat(): JSX.Element {
-    function test(){
-        const prompt = "Name 10 programing technologies";
+    const {register, handleSubmit} = useForm<ChatModel>();
+    function send(chat: ChatModel) {
+
+        const prompt = promptEngineering.getPrompt(chat.tech, chat.num);
         chatGptService.getCompletion(prompt)
-        .then(completion => alert(completion))
-        .catch(err => alert(err.message))
+            .then(completion => alert(completion))
+            .catch(err => alert(err.message))
+
     }
     return (
         <div className="Chat">
-            <form>
+            <form onSubmit={handleSubmit(send)}>
                 <label>Job Interview Subject: </label>
-                <input type="text" />
+                <input type="text" {...register("tech")}/>
                 <label>Number of Questions:</label>
-                <input type="number" min="1" max="20" />
-                <button type="button" onClick={test}>Get Questions</button>
+                <input type="number" min="1" max="20"  {...register("num")}/>
+                <button>Get Questions</button>
             </form>
         </div>
     );
